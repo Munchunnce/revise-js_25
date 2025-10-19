fe6ceed24fd4637971facb1cd19413fd60a9d695
Vimal <vimalkumarak478@gmail.com>
2025-10-19 22:15:10 +0530

    bind, call, apply method Borrowing learn vkc

--- cba/bca.js
+++ cba/bca.js
@@ -1,14 +1,32 @@
+// const user = {
+//     fname: 'Vimal',
+//     lname: 'Kumar',
+//     getFullName: function() {
+//         console.log(this.fname);
+//     }
+// };
+
+// const user2 = {
+//     fname: 'Munchun'
+// };
+
+// const getFullName = user.getFullName.bind(user2);
+// getFullName();
+//Method Borrowing
 const user = {
     fname: 'Vimal',
-    lname: 'Kumar',
-    getFullName: function() {
-        console.log(this.fname);
+    lanme: 'Kumar',
+    getFullName(arg1, arg2) {
+        return `${arg1}, ${arg2}! ${this.fname} ${this.lanme}`;
     }
 };
 
 const user2 = {
-    fname: 'Munchun'
+    fname: 'John',
+    lanme: 'Doe'
 };
 
-const getFullName = user.getFullName.bind(user2);
-getFullName();
+const printName = user.getFullName.call(user2, 'Hi', 'Good Morning');
+console.log(printName);
+console.log(user.getFullName.call(user2, 'Hi', 'Good Morning'));
+console.log(user.getFullName.apply(user2, ['Hi', 'Good Morning']));
